360d616a0d34a63151dbd446b97a4accd47c39d1
kacper.pieron <kpieron@poczta.fm>
2023-05-31 21:39:30 +0200

    testing

--- app/layout.js
+++ app/layout.js
@@ -1,7 +1,8 @@
 import './globals.css'
-import { Inter } from 'next/font/google'
 
-const inter = Inter({ subsets: ['latin'] })
+
+
+
 
 export const metadata = {
   title: 'Create Next App',
@@ -11,7 +12,9 @@ export const metadata = {
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
-      <body className={inter.className}>{children}</body>
+      <body>
+        {children}
+      </body>
     </html>
   )
 }
