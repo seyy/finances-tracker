ddcd853ad4e99cb2f9c237455fadf22db46805cd
kacper.pieron <kpieron@poczta.fm>
2023-06-08 19:36:33 +0200

    updatingCode

--- app/page.js
+++ app/page.js
@@ -11,7 +11,9 @@ import { Doughnut } from "react-chartjs-2";
 // Firebase
 
 import {db} from './lib/firebase/index'
-import {collection, addDoc} from 'firebase/firestore'
+import {collection, addDoc, getDocs, doc, deleteDoc} from 'firebase/firestore'
+
+import { FaRegTrashAlt } from 'react-icons/fa'
 
 ChartJS.register(ArcElement, Tooltip, Legend)
 
@@ -71,6 +73,28 @@ export default function Home() {
     
     try {
       const docSnap = await addDoc(collectionRef, newIncome)
+
+      setIncome(prevState => {
+        return [
+        
+          ...prevState,
+          {
+            id: docSnap.id,
+            ...newIncome,
+          }
+        ]
+      })
+
+    } catch (error) {
+      console.log(error.message)
+    }
+  }
+
+  const deleteIncomeEntryHandler = async (incomeId) => {
+    const docRef = doc(db, 'income', incomeId)
+    try {
+      await deleteDoc(docRef)
+
     } catch (error) {
       console.log(error.message)
     }
@@ -130,6 +154,23 @@ export default function Home() {
       <div className="flex flex-col gap-4 mt-6">
         <h3 className="text-2xl font-bold">Income History:</h3>
 
+        {income.map(i => {
+          return (
+          <div className="flex justify-between item-center" key={i.id}>
+            <div>
+              <p className="font-semibold">{i.description}</p>
+              <small className="text-xs">{i.createdAt.toISOString()}</small>
+            
+            </div>
+            <p className="flex items-center gap-2">
+              {currencyFormatter(i.amount)}
+              <button onClick={() => { deleteIncomeEntryHandler(i.id) }}>
+                <FaRegTrashAlt />
+              </button>  
+            </p>
+          </div>
+          )
+        })}
       </div>
     </Modal>
 
